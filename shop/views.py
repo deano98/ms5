15160b24e031dfd67e0d43b18a8c56977b38dbf4
deano98 <deano_98@hotmail.com>
2022-03-05 14:17:04 +0000

    Add foundation for viewing products on the shop template

--- shop/views.py
+++ shop/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Product
 
 # Create your views here.
 
 def shop(request):
     ''' View to return the shop page '''
-    return render(request, 'shop/shop.html')
+
+    products = Product.objects.all()
+
+    context = {
+        'products': products,
+    }
+
+    return render(request, 'shop/shop.html', context)
