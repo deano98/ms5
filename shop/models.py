db45850452ec2360b7030ae5f7a8bd31e3aeb91f
deano98 <deano_98@hotmail.com>
2022-03-05 13:49:07 +0000

    Customised Admin

--- shop/models.py
+++ shop/models.py
@@ -2,10 +2,14 @@ from django.db import models
 
 
 class Category(models.Model):
+
+    class Meta:
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=254)
     friendly_name = models.CharField(max_length=254, null=True, blank=True)
 
-    def_str_(self):
+    def __str__(self):
         return self.name
     
     def get_friendly_name(self):
