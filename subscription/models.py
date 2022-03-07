9d9ea3ee4de0f85ab0d733f15634afca161027c1
deano98 <deano_98@hotmail.com>
2022-03-07 20:06:26 +0000

    Added subscription app and models

---
+++ subscription/models.py
@@ -0,0 +1,17 @@
+from django.db import models
+
+# Create your models here.
+
+class SubType(models.Model):
+    name = models.CharField(max_length=254)
+    price = models.IntegerField(max_length=50)
+
+    def __str__(self):
+        return self.name
+
+class SubFeatures(models.Model):
+    plan = models.ForeignKey(SubType, on_delete=models.CASCADE)
+    plan_name = models.CharField(max_length=254)
+
+    def __str__(self):
+        return self.plan_name
