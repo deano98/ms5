f53fdefa79d6f937929bcca654c068e4e6142a9f
deano98 <deano_98@hotmail.com>
2022-03-10 22:09:26 +0000

    Add basic BMR calculator

--- subscription/views.py
+++ subscription/views.py
@@ -6,7 +6,18 @@ from .forms import TdeeForm
 
 def subscription(request):
     ''' View to return the subscription page '''
+    tdee = request.POST
+    gender = tdee.get("gender")
+    weight = float(tdee.get("weight"))
+    height = float(tdee.get("height"))
+    age = float(tdee.get("age"))
+    print(tdee)
+    if gender == "male":
+        BMR = 66 + (13.7 * weight) + (5 * height) - (6.8 * age)
+    else:
+        BMR = 655 + (9.6 * weight) + (1.8 * height) - (4.7 * age)
 
+    print(BMR)
 
     return render(request, 'subscription/subscription.html', {
         'tdee_form': TdeeForm(),
